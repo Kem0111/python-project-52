02ceee2d7983bd7cd52fefd5d61dc609c301f916
Камран <kem@192.168.1.13>
2023-04-29 19:14:31 +0300

    fix CI

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import dj_database_url
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -104,8 +103,15 @@ if DJANGO_ENV == "local":
     }
 else:
     DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('POSTGRES_URL'))
-}
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB'),
+            'USER': os.getenv('POSTGRES_USER'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+            'HOST': os.getenv('POSTGRES_HOST'),
+            'PORT': os.getenv('POSTGRES_PORT'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
