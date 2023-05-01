a9b738ebb29f29a198cc441f37cb68964e0f3ace
Камран <kem@192.168.1.13>
2023-05-01 12:13:40 +0300

    fix template

--- core/forms.py
+++ core/forms.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 
 class LoginUserForm(AuthenticationForm):
     username = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control',
+                                      'placeholder': _('Username')})
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                          'placeholder': _('Password')})
     )
